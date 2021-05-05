545667101335a0f60a8584b4f7e87eea8c3a7615
zakharova_da <zakharova.da@dvfu.ru>
2021-05-06 00:10:08 +1000

    add player animation and move

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var clearBtn;
 var player;
 var ctxPlayer;
 
-var gameWidth = 800;
+var gameWidth = 1000;
 var gameHight = 500;
 
 var background = new Image();
@@ -46,12 +46,16 @@ function init()
 	drawBtn.addEventListener("click", drawRect, false);
 	clearBtn.addEventListener("click", clearRect, false);
 
+
 	pl = new Player();
 	enemy = new Enemy();
 
 	drawBg();
 
 	startloop();
+
+	document.addEventListener("keydown", checkKeyDown, false);
+	document.addEventListener("keyup", checkKeyUp, false);
 }
 
 function loop()
@@ -85,6 +89,7 @@ function draw()
 
 function update()
 {
+	pl.update();
 
 }
 
@@ -96,8 +101,12 @@ function Player()
 	this.drawY = 0;
 	this.width = 45;
 	this.height = 90;
+	this.speed = 3;
 
-	this.speed = 5;
+	this.isUp = false;
+	this.isDown = false;
+	this.isRight = false;
+	this.isLeft = false;
 }
 
 function Enemy()
@@ -120,8 +129,82 @@ Enemy.prototype.draw = function ()
 
 Player.prototype.draw = function ()
 {
-	ctxMap.drawImage(tiles, this.srcX, this.srcY, 60, 80,
+	clearCtxPlayer();
+	ctxPlayer.drawImage(tiles, this.srcX, this.srcY, 60, 80,
 		this.drawX, this.drawY, this.width, this.height);
+}
+
+Player.prototype.update = function ()
+{
+	this.chooseDir();
+}
+
+Player.prototype.chooseDir = function ()
+{
+	if(this.isUp)
+		this.drawY -= this.speed;
+	if(this.isDown)
+		this.drawY += this.speed;
+	if(this.isLeft)
+		this.drawX -= this.speed;
+	if(this.isRight)
+		this.drawX += this.speed;
+}
+
+function checkKeyDown (e)
+{
+	var keyID = e.keyCode || e.which;
+	var keyChar = String.fromCharCode(keyID);
+
+	if (keyChar == "W")
+	{
+		pl.isUp = true;
+		e.preventDefault();
+	}
+	if (keyChar == "S")
+	{
+		pl.isDown = true;
+		e.preventDefault();
+	}
+	if (keyChar == "D")
+	{
+		pl.isRight = true;
+		e.preventDefault();
+	}
+	if (keyChar == "A")
+	{
+		pl.isLeft = true;
+		e.preventDefault();
+	}
+
+}
+
+function checkKeyUp (e)
+{
+	var keyID = e.keyCode || e.which;
+	var keyChar = String.fromCharCode(keyID);
+
+	if (keyChar == "W")
+	{
+		pl.isUp = false;
+		e.preventDefault();
+	}
+	if (keyChar == "S")
+	{
+		pl.isDown = false;
+		e.preventDefault();
+	}
+	if (keyChar == "D")
+	{
+		pl.isRight = false;
+		e.preventDefault();
+	}
+	if (keyChar == "A")
+	{
+		pl.isLeft = false;
+		e.preventDefault();
+	}
+
 }
 
 function drawRect ()
@@ -133,6 +216,11 @@ function drawRect ()
 function clearRect ()
 {
 	ctxMap.clearRect(0, 0, 800, 500);
+}
+
+function clearCtxPlayer()
+{
+	ctxPlayer.clearRect(0, 0, gameWidth, gameHight)
 }
 
 function drawBg ()
